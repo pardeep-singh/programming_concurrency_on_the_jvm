19af8bae9b733d294168da85b92ff486413a6f3a
pardeep <pardeep@helpshift.com>
2018-10-23 20:40:04 +0530

    Section 8.8: Typed Actors and Murmurs
    - Add replenish() to EnergySource example.

--- src/main/java/chapter8/EnergySourceImpl.java
+++ src/main/java/chapter8/EnergySourceImpl.java
@@ -1,11 +1,79 @@
 package chapter8;
 
+import akka.actor.Scheduler;
 import akka.actor.TypedActor;
+import scala.Function1;
+import scala.PartialFunction;
+import scala.runtime.AbstractFunction1;
+import scala.PartialFunction$class;
+import java.util.concurrent.TimeUnit;
 
+@SuppressWarnings("unchecked")
 public class EnergySourceImpl extends TypedActor implements EnergySource {
     private final long MAXLEVEL = 100L;
     private long level = MAXLEVEL;
     private long usageCount = 0L;
+
+    class Replenish {}
+
+    @Override
+    public void preStart() {
+        Scheduler.schedule(
+                optionSelf().get(),
+                new Replenish(),
+                1,
+                1,
+                TimeUnit.SECONDS
+        );
+    }
+
+    @Override
+    public void postStop() {
+        Scheduler.shutdown();
+    }
+
+    private void replenish(){
+        System.out.println(
+                "Thread in replenish:" +
+                        Thread.currentThread().getName()
+                );
+        if (level < MAXLEVEL) level += 1;
+    }
+
+    @Override
+    public PartialFunction receive() {
+        return processMessage().orElse(super.receive());
+    }
+
+    private PartialFunction processMessage() {
+        class MyDispatch
+                extends AbstractFunction1
+                implements PartialFunction {
+            public boolean isDefinedAt(Object message) {
+                return message instanceof Replenish;
+            }
+
+            public Object apply(Object message) {
+                if (message instanceof Replenish)
+                    replenish();
+                return null;
+            }
+
+            public Function1 lift() {
+                return PartialFunction$class.lift(this);
+            }
+
+            public PartialFunction andThen(Function1 function) {
+                return PartialFunction$class.andThen(this, function);
+            }
+
+            public PartialFunction orElse(PartialFunction function) {
+                return PartialFunction$class.orElse(this, function);
+            }
+        };
+
+        return new MyDispatch();
+    }
 
     public long getUnitsAvailable() { return level; }
 
